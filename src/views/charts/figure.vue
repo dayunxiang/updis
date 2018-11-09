a6159de6f80e52a6b97d16cf313c4c3ff9c5e662
龚建勋 <gongjianxun@haomo-studio.com>
2018-11-09 17:40:59 +0800

    coding

--- src/views/charts/figure.vue
+++ src/views/charts/figure.vue
@@ -414,7 +414,7 @@
             }
           },
           tooltip : {
-            formatter: '{b} <br> {c} <br/> {a}',
+//            formatter: '{b} <br> {c} <br/> {a}',
             trigger: 'axis',
             /*axisPointer : {    // 坐标轴指示器，坐标轴触发有效
               type : 'shadow',   // 默认为直线，可选为：'line' | 'shadow'
