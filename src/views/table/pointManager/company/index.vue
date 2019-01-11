bdec25a8aed8239eef02c02fac4897689b885cf3
龚建勋 <gongjianxun@haomo-studio.com>
2019-01-11 18:27:02 +0800

    查看/编辑/删除

--- src/views/table/pointManager/company/index.vue
+++ src/views/table/pointManager/company/index.vue
@@ -121,9 +121,9 @@
           label="操作"
           width="150">
           <template slot-scope="scope">
-            <el-button @click="handleClick(scope.row)" type="text" size="small">删除</el-button>
-            <el-button  type="text" @click="dialogFormVisible = true">编辑</el-button>
             <el-button  type="text" @click="viewHadelClick(scope.$index, scope.row)">查看</el-button>
+            <el-button  type="text" @click="dialogFormVisible = true">编辑</el-button>
+            <el-button @click="handleClick(scope.row)" type="text" size="small">删除</el-button>
           </template>
         </el-table-column>
       </el-table>
